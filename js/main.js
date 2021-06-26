0edd30a782b6fe439cf25e182ecf15b3c8620b7b
Fayyaz Alam <fayazalam.98@gmail.com>
2021-06-25 19:10:25 -0700

    set attributes and apend child

--- js/main.js
+++ js/main.js
@@ -28,3 +28,35 @@ function toDo() {
 
 
 
+    btn1.innerHTML = "EDIT"
+    btn2.innerHTML = "DELETE"
+
+
+    btn1.setAttribute("id", "btn1")
+    btn1.setAttribute("onclick", "updt(this)")
+    btn1.setAttribute("class", " btn btn-success")
+
+    btn2.setAttribute("id", "btn2")
+    btn2.setAttribute("onclick", "del(this.parentElement)")
+    btn2.setAttribute("class", "btn btn-danger ")
+
+    inp.setAttribute("id", "listInput")
+    inp.setAttribute("disabled", "disabled")
+    inp.setAttribute("class", "list-input form-control")
+
+    li.setAttribute("id", "li")
+    li.setAttribute("class", "listitem")
+
+    
+
+
+    list.appendChild(li)
+    li.appendChild(inp)
+    li.appendChild(btn1)
+    li.appendChild(btn2)
+
+
+    inp.value = taskValue;
+    mainInput.value = ""
+
+}
